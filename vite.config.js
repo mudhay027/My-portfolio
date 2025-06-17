2da09815eb87c4429df16f0b5871f46eafe7159b
mudhay027 <msudhay2005@gmail.com>
2025-06-17 20:58:58 +0530

    Deployement is finished

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-portfolio/' // 👈 MUST MATCH your GitHub repo name
-})
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+    },
+  },
+});
